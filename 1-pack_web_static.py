230567027752a174767d985d60a5da35d834a7a0
lGikundiro <l.gikundiro@alustudent.com>
2024-10-05 15:07:47 +0200

    Added a static file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-from fabric.api import local
-from time import strftime
+"""
+Fabric script to genereate tgz archive
+execute: fab -f 1-pack_web_static.py do_pack
+"""
+
+from datetime import datetime
+from fabric.api import *
 
 
 def do_pack():
     """
-    Generates a .tgz file from the contents of the web_static folder
-
-    Returns:
-        str: The file path of the generated .tgz file if successful else None.
+    making an archive on web_static folder
     """
 
-    date_time = strftime("%Y%m%d%H%M%S")
-    file_name = "versions/web_static_{}.tgz".format(date_time)
-    try:
-        local("mkdir -p versions")
-        local("tar -zcvf {} web_static".format(file_name))
-        return file_name
-    except Exception as err:
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
         return None
--- models/state.py
+++ models/state.py
@@ -2,26 +2,28 @@
 """ State Module for HBNB project """
 import models
 from models.base_model import BaseModel, Base
-import os
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.city import City
+import os
 
 
 class State(BaseModel, Base):
     """ State class """
-
     __tablename__ = "states"
 
     name = Column(String(128), nullable=False)
 
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref="state",
+        cities = relationship("City", backref="state",
                               cascade="all, delete, delete-orphan")
+
     else:
         @property
         def cities(self):
-            """ Returns the list of City instances with state_id
-            equals to the current State.id. """
-            return [city for city in models.storage.all(City).values()
-                    if city.state_id == self.id]
+            """getter attribute that returns the list of City instances"""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
